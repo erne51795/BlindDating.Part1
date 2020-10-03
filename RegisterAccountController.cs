d890105779c8ffc0f8d0e0c92b262da3e7a5ffdf
erne51795 <66523436+erne51795@users.noreply.github.com>
2020-10-02 23:42:23 -0500

    Add files via upload

---
+++ RegisterAccountController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlindDating.Controllers
+{
+    public class RegisterAccountController : Controller
+    {
+
+        //PROPERTIES
+        private UserManager<IdentityUser> _userManager;
+        private SignInManager<IdentityUser> _signInManager;
+
+        //CONSTRUCTOR
+        public RegisterAccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult CreateUser(string username, string password)
+        {
+            IdentityUser newUser = new IdentityUser();
+            newUser.UserName = username;
+            newUser.Email = username;
+
+            IdentityResult result = _userManager.CreateAsync(newUser, password).Result;
+
+            if (result.Succeeded)
+            {
+                _userManager.AddToRoleAsync(newUser, "NormalUser").Wait();
+
+                _signInManager.SignInAsync(newUser, false).Wait();
+            }
+
+            return RedirectToAction("Create", "DatingProfiles");
+        }
+    }
+
+}
